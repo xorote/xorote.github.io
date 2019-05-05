5a41a01da9efcd0d06cf0028b5c368f733278384
gauthamilango <gauthamilango@gmail.com>
2019-05-05 11:57:13 +0530

    Update

---
+++ CopyrightInfo.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const e = React.createElement;
+
+class CopyrightInfo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+      return "© 2018 - 2019 GIGARAD";
+  }
+}
+
+const domContainer = document.querySelector('#copyright_info_container');
+ReactDOM.render(e(CopyrightInfo), domContainer);
